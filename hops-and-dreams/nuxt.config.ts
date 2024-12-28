ee88dac9b030bf79feede61d101d417c4251aa0f
moaberglund <moa.21@hotmail.com>
2024-12-28 11:25:16 +0100

    mongoose

--- hops-and-dreams/nuxt.config.ts
+++ hops-and-dreams/nuxt.config.ts
@@ -2,5 +2,11 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ['nuxt-mongoose']
+  modules: ['nuxt-mongoose'],
+  mongoose: {
+    uri: 'process.env.MONGODB_URI',
+    options: {},
+    modelsDir: 'models',
+    devtools: true,
+  },
 })
