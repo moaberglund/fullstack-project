ce5f60f04976b22b4a9ab79c95d6719ec699fcb1
JoeSteeloak <141022479+JoeSteeloak@users.noreply.github.com>
2024-12-28 12:05:01 +0100

    add mongoose model co-authored Moa

---
+++ hops-and-dreams/server/models/beverage.model.ts
@@ -0,0 +1,64 @@
+import { defineMongooseModel } from '#nuxt/mongoose'
+
+export const User = defineMongooseModel('Beverage', {
+  category: {
+    type: String,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  producer: {
+    type: String,
+    required: true,
+  },
+  country: {
+    type: String,
+    required: true,
+  },
+  region: {
+    type: String,
+    required: true,
+  },
+  year: {
+    type: Number,
+    required: true,
+  },
+  volume: {
+    type: Number,
+    required: true,
+  },
+  alcohol_by_volume: {
+    type: Number,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  organic: {
+    type: Boolean,
+    required: true,    
+  }, 
+  vegan: {
+    type: Boolean,
+    required: true,
+  },
+  shelf_amount: {
+    type: Number,
+    required: true,
+  },
+  shelf_id: {
+    type: Number,
+    required: true,
+  },
+}, {
+
+}, (schema) => {
+
+})
