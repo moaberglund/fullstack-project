b570123575a5b60c84fc79ff928f7c53c745c9ed
JoeSteeloak <141022479+JoeSteeloak@users.noreply.github.com>
2024-12-28 12:11:45 +0100

    add user model co-author Moa

---
+++ hops-and-dreams/server/models/user.model.ts
@@ -0,0 +1,26 @@
+import { defineMongooseModel } from '#nuxt/mongoose'
+
+export const User = defineMongooseModel('User', {
+    fullname: {
+        type: String,
+        required: true,
+    },
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    role: {
+        type: String,
+        required: true,
+    },
+  
+}, {
+
+}, (schema) => {
+
+})
