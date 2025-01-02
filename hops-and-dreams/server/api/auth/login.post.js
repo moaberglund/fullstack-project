4f2fc594ac2189bec768b5a5ebcdd05e2296a3af
JoeSteeloak <141022479+JoeSteeloak@users.noreply.github.com>
2025-01-02 11:13:39 +0100

    convert to mongoose

--- hops-and-dreams/server/api/auth/login.post.js
+++ hops-and-dreams/server/api/auth/login.post.js
@@ -1,44 +1,47 @@
-import { PrismaClient } from '@prisma/client';
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
+import { User } from '~/server/models/user.schema'; // Importera din schema-modell
 
-const prisma = new PrismaClient();
-const JWT_SECRET = 'your_token'; // Replace with your secret key
+const JWT_SECRET = 'your_token'; // Byt ut mot din hemliga nyckel
 
 export default defineEventHandler(async (event) => {
     const { username, password } = await readBody(event);
 
-    // Find the user in the database
-    const user = await prisma.users.findUnique({
-        where: { Username: username }
-    });
+    try {
+        // Hitta användaren i databasen
+        const user = await User.findOne({ username });
 
-    if (!user) {
-        return { statusCode: 401, body: { success: false, message: 'Invalid username or password' } };
-    }
-
-    // Check if the password is correct
-    const validPassword = await bcrypt.compare(password, user.Password);
-
-    if (!validPassword) {
-        return { statusCode: 401, body: { success: false, message: 'Invalid username or password' } };
-    }
-
-    // Create a JWT token
-    const token = jwt.sign(
-        { id: user.Id, username: user.Username },
-        JWT_SECRET,
-        { expiresIn: '12h' } // Token expires in 12 hour
-    );
-
-
-    // Return the token to the frontend
-    return {
-        statusCode: 200,
-        body: {
-            success: true,
-            message: 'Login successful',
-            token
+        if (!user) {
+            return { statusCode: 401, body: { success: false, message: 'Invalid username or password' } };
         }
-    };
+
+        // Kontrollera om lösenordet är korrekt
+        const validPassword = await bcrypt.compare(password, user.password);
+
+        if (!validPassword) {
+            return { statusCode: 401, body: { success: false, message: 'Invalid username or password' } };
+        }
+
+        // Skapa en JWT-token
+        const token = jwt.sign(
+            { id: user._id, username: user.username, role: user.role },
+            JWT_SECRET,
+            { expiresIn: '12h' } // Token går ut om 12 timmar
+        );
+
+        // Returnera token till frontend
+        return {
+            statusCode: 200,
+            body: {
+                success: true,
+                message: 'Login successful',
+                token
+            }
+        };
+    } catch (error) {
+        return {
+            statusCode: 500,
+            body: { success: false, message: 'Internal server error', error: error.message }
+        };
+    }
 });
