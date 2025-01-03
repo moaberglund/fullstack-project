405e410db973ba35558398219dfed1af5662bc13
moaberglund <moa.21@hotmail.com>
2025-01-03 12:00:19 +0100

    liten bubbla för kategori

---
+++ hops-and-dreams/components/MenuBubble.vue
@@ -0,0 +1,53 @@
+<template>
+
+    <div class="menu-bubble">
+
+        <div class="category-img"></div>
+        <div class="category-name">
+            <h2>{{  category }}</h2>
+        </div>
+        
+    </div>
+
+
+</template>
+
+<script>
+
+export default {
+    name: 'MenuBubble',
+    props: {
+        category: String
+    }
+}
+
+</script>
+
+<style scoped>
+
+.menu-bubble {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    width: 125px;
+    height: 200px;
+    background-color: #f0f0f0;
+    margin: 10px;
+    border-radius: 25px;
+}
+
+.category-img {
+    width: 100px;
+    height: 100px;
+    background-color: #c06363;
+    border-radius: 20%;
+}
+
+h2 {
+    padding: 1em;
+    font-size: 1.2em;;
+}
+
+
+</style>
---
+++ hops-and-dreams/pages/test.vue
@@ -0,0 +1,11 @@
+<template>
+    <h1>Test page</h1>
+
+    <MenuBubble category="Wine" />
+
+</template>
+
+<script>
+import MenuBubble from '~/components/MenuBubble.vue';
+
+</script>
