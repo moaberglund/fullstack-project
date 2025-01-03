9cc1f8bc4aae6b1dee74a667321b3ed3e1d3c51f
JoeSteeloak <141022479+JoeSteeloak@users.noreply.github.com>
2025-01-03 11:03:53 +0100

    try allowing commits in live share, not working
    Co-authored-by: Moa Berglund <moaberglund@users.noreply.github.com>

--- hops-and-dreams/nuxt.config.ts
+++ hops-and-dreams/nuxt.config.ts
@@ -1,14 +1,32 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-plugins: ['~/plugins/fontawesome.js'],
+  //Bootstrap
+  app: {
+    head: {
+      link: [
+        {
+          rel: 'stylesheet',
+          href: 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css'
+        }
+      ],
+      script: [
+        {
+          src: 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js',
+          tagPosition: 'bodyClose'
+        }
+      ]
+    }
+  },
+  css: ['@/assets/css/main.css'],
+  plugins: ['~/plugins/fontawesome.js'],
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  devServer: {port: 8000},
+  devServer: { port: 8000 },
   modules: ['nuxt-mongoose', '@nuxt/image'],
   mongoose: {
     uri: process.env.MONGODB_URI,
     options: {},
     modelsDir: 'models',
     devtools: true,
-  },
+  }, 
 })
