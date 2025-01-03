96912e7814f54b6b1e570f0e755dc970638388fe
moaberglund <moa.21@hotmail.com>
2025-01-03 16:09:26 +0100

    rundade hörn

--- hops-and-dreams/components/AppHeader.vue
+++ hops-and-dreams/components/AppHeader.vue
@@ -1,29 +1,28 @@
 <template>
-<header>
-<h1>Inventory</h1>
-<input type="text" placeholder="  Search for products">
-
-</header>
+  <header>
+    <h1>Inventory</h1>
+    <input type="text" placeholder="  Search for products" />
+  </header>
 </template>
 
 <script>
-
 export default {
-name: "Header",
-
-
+  name: "Header",
 };
 </script>
 
 <style scoped>
 header {
-background-color: #FFF9EF;
-padding: 1em;
+  border-radius: 0 0 25px 25px;
+}
+header {
+  background-color: #fff9ef;
+  padding: 1em;
 }
 
 input {
-padding: 1em;
-border-radius: 25px;
-background-color: #EDEDED;
+  padding: 1em;
+  border-radius: 25px;
+  background-color: #ededed;
 }
 </style>
