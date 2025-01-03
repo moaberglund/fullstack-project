a741205151b1e2fdd672878ee828d35d1dd4bdab
moaberglund <moa.21@hotmail.com>
2025-01-03 16:16:32 +0100

    container

--- hops-and-dreams/components/AppFooter.vue
+++ hops-and-dreams/components/AppFooter.vue
@@ -1,7 +1,13 @@
 <template>
-  <footer class="d-flex justify-content-between align-items-center p-2 bg-light rounded-top">
-    <button @click="home" id="btn-home" class="btn btn-primary">Home</button>
-    <button @click="back" id="btn-back" class="btn btn-secondary">Back</button>
+  <footer class="bg-light rounded-top">
+    <div
+      class="container d-flex justify-content-between align-items-center py-2"
+    >
+      <button @click="home" id="btn-home" class="btn btn-primary">Home</button>
+      <button @click="back" id="btn-back" class="btn btn-secondary">
+        Back
+      </button>
+    </div>
   </footer>
 </template>
 
@@ -15,28 +21,28 @@ export default {
     },
     back() {
       // Hämta den aktuella URL:en och dela upp den i delar
-      const pathParts = this.$route.path.split('/');
+      const pathParts = this.$route.path.split("/");
 
-      // Ta bort sista delen av URL:en 
+      // Ta bort sista delen av URL:en
       pathParts.pop();
 
       // Kolla om vi redan är på startsidan
       if (pathParts.length > 1) {
         // Om vi inte är på startsidan, navigera upp ett steg
-        const newPath = pathParts.join('/');
+        const newPath = pathParts.join("/");
         this.$router.push(newPath);
       } else {
         // Om vi är på startsidan, gå tillbaka till startsidan
-        this.$router.push('/');
+        this.$router.push("/");
       }
-    }
+    },
   },
 };
 </script>
 
 <style scoped>
 footer {
-  height: 50px;
+  height: 80px;
   background-color: #ececec;
   border-radius: 25px 25px 0 0;
 }
