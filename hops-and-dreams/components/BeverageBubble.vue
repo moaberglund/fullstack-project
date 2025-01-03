fe6842fa85617814c5282bde0c8c61e0e74514b6
moaberglund <moa.21@hotmail.com>
2025-01-03 13:19:56 +0100

    start

---
+++ hops-and-dreams/components/BeverageBubble.vue
@@ -0,0 +1,52 @@
+<template>
+
+    <div class="begerage-bubble">
+
+        <div class="beverage-img"></div>
+
+        <h1>{{name}}</h1>
+
+        <!-- if vegan + organic ? -->
+
+        <h2>{{producer}}  {{year}}</h2>
+
+        <h3>{{country}} {{region}}</h3>
+        <h3>{{volume}} {{alcohol_by_volume}}</h3>
+
+        <p class="price">{{price}}</p>
+
+        <p class="description">{{description}}</p>
+
+        <p>Shelf: {{shelf_id}}</p>
+
+        <p>Amount:</p>
+        <div class="amount">
+            <button>-</button>
+            <p>{{amount}}</p>
+            <button>+</button>
+        </div>
+
+    </div>
+
+</template>
+
+<script>
+
+export default {
+    name: 'BeverageBubble',
+    props: {
+        name: String,
+        producer: String,
+        year: Number,
+        country: String,
+        region: String,
+        volume: Number,
+        alcohol_by_volume: Number,
+        price: Number,
+        description: String,
+        shelf_id: Number,
+        amount: Number
+    }
+}
+
+</script>
