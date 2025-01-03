07e42b6a388eeb976b4b735376de0ee8d2b57a13
moaberglund <moa.21@hotmail.com>
2025-01-03 15:54:28 +0100

    bootstrap

--- hops-and-dreams/pages/[category]/[subcategory]/index.vue
+++ hops-and-dreams/pages/[category]/[subcategory]/index.vue
@@ -1,18 +1,47 @@
 <template>
-    <section>
-        <div class="block">
-            <h1>{{ category }} - {{ subcategory }} Subcategory</h1>
-            <ul>
-                <li v-for="beverage in filteredBeverages" :key="beverage._id">
-                    <!-- Länk till detaljerad sida för varje dryck -->
-                    <nuxt-link :to="`/${category}/${subcategory}/${beverage._id}`">
-    {{ beverage.name }}
-</nuxt-link>
-
-                </li>
-            </ul>
+ <section>
+    <div class="block container">
+      <div class="row">
+        <div class="col-12 text-center mb-4">
+          <h1>{{ category }} - {{ subcategory }} Subcategory</h1>
         </div>
-    </section>
+      </div>
+
+      <div class="row">
+        <div
+          class="col-12 col-md-4 col-lg-3 mb-3"
+          v-for="beverage in filteredBeverages"
+          :key="beverage._id"
+        >
+          <div class="card text-center p-3 shadow-sm">
+            <div
+              class="rounded mx-auto mb-3"
+              :style="{
+                width: '100px',
+                height: '100px',
+                backgroundColor:
+                  beverage.category === 'Wine'
+                    ? '#f8d7da'
+                    : beverage.category === 'Beer'
+                    ? '#d4edda'
+                    : beverage.category === 'Cider'
+                    ? '#FAEDCB'
+                    : beverage.category === 'Mixed Drinks'
+                    ? '#D5C8E9'
+                    : beverage.category === 'Spirits'
+                    ? '#F8D9C4'
+                    : '#cce5ff',
+              }"
+            ></div>
+            <!-- Länk till den detaljerade sidan för drycken -->
+            <nuxt-link :to="`/${category}/${subcategory}/${beverage._id}`">
+              {{ beverage.name }}
+            </nuxt-link>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
