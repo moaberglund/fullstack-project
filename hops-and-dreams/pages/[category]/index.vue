a38bf2b035e65fa072a8337feae7f05904133ff1
moaberglund <moa.21@hotmail.com>
2025-01-03 15:21:26 +0100

    styling för category

--- hops-and-dreams/pages/[category]/index.vue
+++ hops-and-dreams/pages/[category]/index.vue
@@ -1,18 +1,45 @@
 <template>
-    <section>
-        <div class="block">
-            <h1>{{ category }} Category</h1>
-
-            <!-- Lista över subkategorier som finns i den valda kategorin -->
-            <ul>
-                <li v-for="subcategory in uniqueSubcategories" :key="subcategory">
-                    <!-- Länk till subkategori-sidan -->
-                    <nuxt-link :to="`/${category}/${subcategory}`">{{ subcategory }}</nuxt-link>
-                </li>
-            </ul>
-
+  <section>
+    <div class="block container">
+      <div class="row">
+        <div class="col-12 text-center mb-4">
+          <h1>{{ category }} Category</h1>
         </div>
-    </section>
+      </div>
+
+      <div class="row">
+        <div
+          class="col-12 col-md-4 col-lg-3 mb-3"
+          v-for="subcategory in uniqueSubcategories"
+          :key="subcategory"
+        >
+          <div class="card text-center p-3 shadow-sm">
+            <div
+              class="rounded mx-auto mb-3"
+              :style="{
+                width: '100px',
+                height: '100px',
+                backgroundColor:
+                  category === 'Wine'
+                    ? '#f8d7da'
+                    : category === 'Beer'
+                    ? '#d4edda'
+                    : category === 'Cider'
+                    ? '#FAEDCB'
+                    : category === 'Mixed Drinks'
+                    ? '#D5C8E9'
+                    : category === 'Spirits'
+                    ? '#F8D9C4'
+                    : '#cce5ff',
+              }"
+            ></div>
+            <!-- Länk till subkategori-sidan -->
+            <nuxt-link :to="`/${category}/${subcategory}`">{{ subcategory }}</nuxt-link>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
