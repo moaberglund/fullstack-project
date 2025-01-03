3dd874b4d2df2dee843b1e01756a02ad2b6c150e
moaberglund <moa.21@hotmail.com>
2025-01-03 13:56:59 +0100

    styling

--- hops-and-dreams/components/BeverageBubble.vue
+++ hops-and-dreams/components/BeverageBubble.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="card beverage-bubble text-center mx-auto p-3">
+  <div class="card beverage-bubble mx-auto p-3">
     <div class="beverage-img bg-danger rounded"></div>
 
     <div class="card-body beverage-text">
@@ -10,17 +10,17 @@
       <h3 class="h6 text-muted">{{ country }}, {{ region }}</h3>
       <h3 class="h6 text-muted">{{ volume }} - {{ alcohol_by_volume }}</h3>
 
-      <p class="price h4 text-primary">&euro;{{ price }}</p>
+      <p class="price h4">&euro;{{ price }}</p>
 
       <p class="description text-muted">{{ description }}</p>
 
       <p class="shelf text-muted">Shelf: {{ shelf_id }}</p>
 
-      <div class="d-flex align-items-center justify-content-center gap-3">
+      <div class="amount">
         <p class="m-0">Amount:</p>
         <div class="input-group amount">
           <button class="btn btn-outline-secondary">-</button>
-          <p class="m-0 px-3">{{ amount }}</p>
+          <p class="m-0 px-3 amount-btn">{{ amount }}</p>
           <button class="btn btn-outline-secondary">+</button>
         </div>
       </div>
@@ -58,4 +58,20 @@ export default {
   width: 100%;
   height: 280px;
 }
+h3 {
+  font-size: 12px;
+}
+.description {
+  font-size: 12px;
+}
+
+.amount-btn {
+    padding-top: 5px;
+    border-bottom: 1px solid black;
+    border-top: 1px solid black;
+    font-size: 16px;
+    font-weight: bold;
+}
+
+
 </style>
