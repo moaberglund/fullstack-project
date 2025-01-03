9873da8e8749e9144101e571825f2a3e8e3cc2b9
moaberglund <moa.21@hotmail.com>
2025-01-03 13:43:10 +0100

    home knapp

--- hops-and-dreams/components/AppFooter.vue
+++ hops-and-dreams/components/AppFooter.vue
@@ -1,28 +1,25 @@
 <template>
-<footer>
-
-    
-    <button id="btn-home">Home</button>
+  <footer>
+    <button @click="home" id="btn-home">Home</button>
     <button id="btn-back">Back</button>
-
-</footer>
+  </footer>
 </template>
 
 <script>
-
 export default {
-name: "Footer",
-
-
+  name: "Footer",
+  methods: {
+    home() {
+      // Redirect the user to the home page
+      this.$router.push("/");
+    },
+  },
 };
 </script>
 
 <style scoped>
-
 footer {
-    background-color: #ECECEC;
-    border-radius: 25px 25px 0 0;
+  background-color: #ececec;
+  border-radius: 25px 25px 0 0;
 }
-
-
 </style>
